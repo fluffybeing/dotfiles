6f5369be61f2c143edb659a35d3fbe07a05db349
Rahul Ranjan <rahul@Rahuls-MacBook-Pro.local>
2017-04-09 03:51:52 +0530

    update setting to current system setting

--- bootstrap_new_system.sh
+++ bootstrap_new_system.sh
@@ -10,59 +10,59 @@ dev="$HOME/Code/dotfiles"
 # If we on OS X, install homebrew and tweak system a bit.
 if [[ `uname` == 'Darwin' ]]; then
 
-    # ##############################################
-    # # Xcode Command Line                         #
-    # ##############################################
-    # sh $dev/osx/xcode_setup.sh
+    ##############################################
+    # Xcode Command Line                         #
+    ##############################################
+    sh $dev/osx/xcode_setup.sh
 
-    # #############################################
-    # # Homebrew                                  #
-    # #############################################
-    # which -s brew
-    # if [[ $? != 0 ]]; then
-    #   echo 'Installing Homebrew...'
-    #     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install)"
-    #     brew update
-    #     brew tap Homebrew/bundle
-    #     brew bundle
-    # fi
-    # #############################################
-    # # Zsh                                       #
-    # #############################################
-    # git clone https://github.com/rupa/z.git ~/z
-    # chmod +x ~/z/z.sh
+    #############################################
+    # Homebrew                                  #
+    #############################################
+    which -s brew
+    if [[ $? != 0 ]]; then
+      echo 'Installing Homebrew...'
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install)"
+        brew update
+        brew tap Homebrew/bundle
+        brew bundle
+    fi
+    #############################################
+    # Zsh                                       #
+    #############################################
+    git clone https://github.com/rupa/z.git ~/z
+    chmod +x ~/z/z.sh
 
-    # ##############################################
-    # # Pygments & Pip                             #
-    # ##############################################
-    # sudo easy_install Pygments
-    # sudo easy_install pip
+    ##############################################
+    # Pygments & Pip                             #
+    ##############################################
+    sudo easy_install Pygments
+    sudo easy_install pip
 
-    # ##############################################
-    # # Symlinks                                  #
-    # ##############################################
-    # ln -fs "$dev/git/gitattributes"
-    # ln -fs "$dev/git/gitconfig"
-    # ln -fs "$dev/git/gitignore"
+    ##############################################
+    # Symlinks                                  #
+    ##############################################
+    ln -fs "$dev/git/gitattributes"
+    ln -fs "$dev/git/gitconfig"
+    ln -fs "$dev/git/gitignore"
 
-    # ##############################################
-    # # OSX                                        #
-    # ##############################################
-    # sh $dev/osx/sensible_defaults.sh
+    ##############################################
+    # OSX                                        #
+    ##############################################
+    sh $dev/osx/sensible_defaults.sh
 
 
-    # ##############################################
-    # # Oh My ZSH                                  #
-    # ##############################################
-    # if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    #   if [[ ! -d $dir/oh-my-zsh/ ]]; then
-    #     sh -c "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
-    #   fi
-    # fi
+    ##############################################
+    # Oh My ZSH                                  #
+    ##############################################
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+      if [[ ! -d $dir/oh-my-zsh/ ]]; then
+        sh -c "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
+      fi
+    fi
 
-    # if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-    #   chsh -s $(which zsh)
-    # fi
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+      chsh -s $(which zsh)
+    fi
 
     # setting
     ln -s $dev/zsh/rahul.zsh-theme $HOME/.oh-my-zsh/themes
@@ -91,37 +91,37 @@ if [[ `uname` != 'Darwin' ]]; then
 fi
 
 
-# # For MacOS
-# if [[ `uname` == 'Darwin' ]]; then
-#   echo 'Enter new hostname of the machine (e.g. macbook-rahul)'
-#     read hostname
-#     echo "Setting new hostname to $hostname..."
-#     scutil --set HostName "$hostname"
-#     compname=$(sudo scutil --get HostName | tr '-' '.')
-#     echo "Setting computer name to $compname"
-#     scutil --set ComputerName "$compname"
+# For MacOS
+if [[ `uname` == 'Darwin' ]]; then
+  echo 'Enter new hostname of the machine (e.g. macbook-rahul)'
+    read hostname
+    echo "Setting new hostname to $hostname..."
+    scutil --set HostName "$hostname"
+    compname=$(sudo scutil --get HostName | tr '-' '.')
+    echo "Setting computer name to $compname"
+    scutil --set ComputerName "$compname"
 
-#   echo 'Checking for SSH key, generating one if it does not exist...'
-#     [[ -f '~/.ssh/id_rsa.pub' ]] || ssh-keygen -t rsa
+  echo 'Checking for SSH key, generating one if it does not exist...'
+    [[ -f '~/.ssh/id_rsa.pub' ]] || ssh-keygen -t rsa
 
-#   echo 'Copying public key to clipboard. Paste it into your Github account...'
-#     [[ -f '~/.ssh/id_rsa.pub' ]] && cat '~/.ssh/id_rsa.pub' | pbcopy
-#     open 'https://github.com/account/ssh'
+  echo 'Copying public key to clipboard. Paste it into your Github account...'
+    [[ -f '~/.ssh/id_rsa.pub' ]] && cat '~/.ssh/id_rsa.pub' | pbcopy
+    open 'https://github.com/account/ssh'
 
-#   open_apps() {
-#     echo 'Install apps:'
-#     echo 'Dropbox:'
-#     open https://www.dropbox.com
-#     echo 'Chrome:'
-#     open https://www.google.com/intl/en/chrome/browser/
-#     echo 'VLC:'
-#     open http://www.videolan.org/vlc/index.html
-#   }
+  open_apps() {
+    echo 'Install apps:'
+    echo 'Dropbox:'
+    open https://www.dropbox.com
+    echo 'Chrome:'
+    open https://www.google.com/intl/en/chrome/browser/
+    echo 'VLC:'
+    open http://www.videolan.org/vlc/index.html
+  }
 
 
-#   echo 'Should I give you links for system applications (e.g. Skype, Tower, VLC)?'
-#   echo 'n / y'
-#   read give_links
-#   [[ "$give_links" == 'y' ]] && open_apps
-#   popd
-# fi
+  echo 'Should I give you links for system applications (e.g. Skype, Tower, VLC)?'
+  echo 'n / y'
+  read give_links
+  [[ "$give_links" == 'y' ]] && open_apps
+  popd
+fi
