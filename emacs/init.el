afe8ab3d7fdbec06e92e0189af7419492680a6bf
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-06-05 12:15:52 +0800

    udpate the emacs init file to use package manager

--- emacs/init.el
+++ emacs/init.el
@@ -3,81 +3,164 @@
 
 ;; INSTALL PACKAGES
 ;; --------------------------------------
+(setq inhibit-startup-message t)
 
 (require 'package)
-
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
        '("melpa" . "http://melpa.org/packages/") t)
-
+;; keep the installed packages in .emacs.d
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
-(defvar myPackages
-  '(better-defaults
-    ein
-    elpy
-    flycheck
-    material-theme
-    py-autopep8
-    projectile
-    swift-mode))
+;; Configurations
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
+;; smart tab behavior - indent or complete
+(setq tab-always-indent 'complete)
 
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
+(setq user-full-name "Rahul Ranjan"
+      user-mail-address "rahul@rudrakos.com")
 
-(setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
-(global-linum-mode t) ;; enable line numbers globally
-(setq linum-format "%d ")
+;; Always load newest byte code
+(setq load-prefer-newer t)
 
-;; PROJECTILE CONFIGURATION
-;; --------------------------------------
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
 
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
 
-;; PYTHON CONFIGURATION
-;; --------------------------------------
+;; more useful frame title, that show either a file or a
+;; buffer name (if the buffer isn't visiting a file)
+(setq frame-title-format
+      '((:eval (if (buffer-file-name)
+                   (abbreviate-file-name (buffer-file-name))
+                 "%b"))))
 
-(setq elpy-rpc-python-command "python3")
-(setq python-shell-interpreter "python3")
-(elpy-enable)
+;; Keep modes indentation
+(setq-default indent-tabs-mode nil)   ;; don't use tabs to indent
+(setq-default tab-width 8)            ;; but maintain correct appearance
 
+;; smart tab behavior - indent or complete
+(setq tab-always-indent 'complete)
 
-;; use flycheck not flymake with elpy
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;; Keep the garbage collection in check
+(setq gc-cons-threshold 20000000)
 
-;; enable autopep8 formatting on save
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; Newline at end of file
+(setq require-final-newline t)
 
-;; The following lines are always needed.  Choose your own keys.
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-switchb)
+;; Wrap lines at 80 characters
+(setq-default fill-column 80)
+;; No blink cursor
+(blink-cursor-mode -1)
 
-;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (projectile py-autopep8 material-theme flycheck elpy ein better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; delete the selection with a keypress
+(delete-selection-mode t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(defconst rahul-savefile-dir (expand-file-name "savefile" user-emacs-directory))
+
+;; create the savefile dir if it doesn't exist
+(unless (file-exists-p rahul-savefile-dir)
+  (make-directory rahul-savefile-dir))
+
+;; revert buffers automatically when underlying files are changed externally
+(global-auto-revert-mode t)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-verbose t)
+
+;; Packages
+
+;;; built-in
+(use-package paren
+  :config
+  (show-paren-mode +1))
+
+(use-package elec-pair
+  :config
+  (electric-pair-mode +1))
+
+;; highlight the current line
+(use-package hl-line
+  :config
+  (global-hl-line-mode +1))
+
+;; saveplace remembers your location in a file when saving files
+(use-package saveplace
+  :config
+  (setq save-place-file (expand-file-name "saveplace" rahul-savefile-dir))
+  ;; activate it for all buffers
+  (setq-default save-place t))
+
+;; Helps you to try a package without installing it
+(use-package try
+  :ensure t)
+
+;;; third-party packages
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t))
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
+
+(use-package ag
+  :ensure t)
+
+(use-package projectile
+  :ensure t
+  :init
+  (setq projectile-completion-system 'ivy)
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (projectile-mode +1))
+
+;; Which next key you need to type
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+;; Apple Swift
+(use-package swift-mode
+  :ensure t
+  :config
+  '(setq swift-mode:basic-offset 2))
+
+(use-package flycheck-swift
+  :ensure t
+  :config
+  '(eval-after-load 'flycheck '(flycheck-swift-setup)))
+
+;; config changes made through the customize UI will be stored here
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(when (file-exists-p custom-file)
+  (load custom-file))
