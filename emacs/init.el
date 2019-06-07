7060c458a15857d566f9dce31ef3d34701a54798
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-06-07 11:45:57 +0800

    Add shell pop up

--- emacs/init.el
+++ emacs/init.el
@@ -205,6 +205,15 @@
   (("C-c C-q" . quickrun)
    ("<f8>" . quickrun-compile-only)))
 
+(use-package shell-pop
+  :ensure t
+  :bind (("C-t" . shell-pop))
+  :config
+  (setq shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+  (setq shell-pop-term-shell "/bin/zsh")
+  ;; need to do this manually or not picked up by `shell-pop'
+  (shell-pop--set-shell-type 'shell-pop-shell-type shell-pop-shell-type))
+
 ;; config changes made through the customize UI will be stored here
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
