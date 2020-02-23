a73a76a6990f72513aca0bce285f233a2c8f416b
Rahul Ranjan <rahul.rrixe@gmail.com>
2020-02-23 15:59:37 +0800

    Add doom emacs themes

--- emacs/init.el
+++ emacs/init.el
@@ -233,19 +233,30 @@
 (use-package diminish
   :ensure t)
 
-(use-package smart-mode-line
+(use-package doom-modeline
   :ensure t
-  :init (add-hook 'after-init-hook 'sml/setup)
-  :config
-  (setq sml/no-confirm-load-theme t)
-  (setq sml/theme 'respectful))
+  :init (doom-modeline-mode 1))
 
 ;; Themes
-(use-package dracula-theme
+(use-package doom-themes
   :ensure t
   :config
-  (if (display-graphic-p)
-    (load-theme 'dracula t)))
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+  
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 (use-package dash
   :config (dash-enable-font-lock))
