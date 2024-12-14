345f67006af753469b44000b7b5e733899fe8639
Rahul Ranjan <rahul.rrixe@gmail.com>
2024-12-14 22:58:25 +0100

    use xcodebuild in neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -81,4 +81,21 @@ return {
     },
     config = true,
   },
+
+  {
+    "wojciech-kulik/xcodebuild.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-tree.lua", -- (optional) to manage project files
+      "stevearc/oil.nvim", -- (optional) to manage project files
+      "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+    },
+    config = function()
+      require("xcodebuild").setup({
+        -- put some options here or leave it empty to use default settings
+      })
+    end,
+  },
 }
